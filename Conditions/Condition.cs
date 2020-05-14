61d6d5914f2dd9a6e8595de62fada7735bf28a23
bastare <noobfrizz1@gmail.com>
2020-05-14 23:34:04 +0300

    Complet 'condition' section

--- Conditions/Condition.cs
+++ Conditions/Condition.cs
@@ -1,9 +1,86 @@
 namespace gagaga.Conditions
 {
-    public static class Condition
+    public static class Conditions
     {
-        public static void IfCondition () { }
+        const bool TRUE_BOOL = true;
+        const bool FALSE_BOOL = false;
 
-        public static void IfElseCondition () { }
+        const int THREE = 3;
+        const int FIVE = 5;
+        public static void Condition()
+        {
+            {
+                if (THREE == 4)
+                {
+                    System.Console.WriteLine("If contidions return 'true', do it");
+                }
+                else if (FIVE < 1)
+                {
+                    System.Console.WriteLine("If contidions return 'true', do it");
+                }
+                else
+                {
+                    System.Console.WriteLine("If no one condition return 'true', do it");
+                }
+            }
+
+            {
+
+                //Shorthand, if code block is single line
+                if (THREE == 4)
+                    System.Console.WriteLine("If contidions return 'true', do it");
+                else if (FIVE < 1)
+                    System.Console.WriteLine("If contidions return 'true', do it");
+                else
+                    System.Console.WriteLine("If no one condition return 'true', do it");
+            }
+
+            {
+                //Same
+                if (THREE == 4 && FIVE < 1)
+                    System.Console.WriteLine("If contidions return 'true', do it");
+                else
+                    System.Console.WriteLine("If no one condition return 'true', do it");
+            }
+
+            {
+                //Just do it, if condition successed 
+                if (THREE == 4 && FIVE < 1)
+                    System.Console.WriteLine("If contidions return 'true', do it");
+            }
+        }
+
+        public static void ConditionDatReturnSmt()
+        {
+            {
+
+                var result = default(string); /* same as 
+                string result = null;            */
+
+                if (THREE == 3)
+                    result = $"Yeap, value of {nameof(THREE)} is 3";
+                else if (THREE < 4)
+                    result = $"Yeap, value of {nameof(THREE)} is lesser then 4";
+                else
+                    result = "I dunno";
+
+                //Or
+                if (THREE == 3)
+                    result = $"Value of {nameof(THREE)} its 3";
+                else
+                    result = "No it doesn`t";
+            }
+
+            {
+                //Same
+                string result = THREE == 3
+                            ? $"Yeap value of {nameof(THREE)} is 3"
+                                : THREE < 4
+                                    ? $"Yeap value of {nameof(THREE)} is lesser then 4"
+                                    : "I dunno";
+                //Or
+                result = THREE == 3 ? $"Value of {nameof(THREE)} its 3" : "No it doesn`t";
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,31 +5,18 @@ namespace HelloApp
 
     class Program
     {
-        // метод Main - стартовая точка приложения
+
         static void Main (string[] args)
         {
-            // int[] age = new int[3] { 1, 2, 3 };
-            // foreach (var item in age)
-            //     System.Console.WriteLine(item);
-
             var result = Task2 (new string[] { "пича", "воле" });
-
         }
-
-        // static int Task1(int a, string b)
-        // {
-        //     var qqq = 12;
-        //     return qqq;
-        // }
 
         static string Task2 (string[] aaa)
         {
             string vad = string.Empty;
 
             for (int i = 0; i < aaa.Length; i++)
-            {
                 vad += aaa[i];
-            }
 
             return vad;
         }
